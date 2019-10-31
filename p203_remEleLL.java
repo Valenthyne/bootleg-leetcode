688591214e87f884d3d4204d5b1b323987acb730
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-10-30 19:03:00 -0500

    Added comments

--- p203_remEleLL.java
+++ p203_remEleLL.java
@@ -10,19 +10,28 @@
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         
+        // Start at beginning of list
         ListNode cur = head;
         ListNode prev = null;
         
+        // Begin cycling throughout the list
         while (cur != null) {
             
+            // Store the value of the current node in v
             int v = cur.val;
             
+            // Case 1: Value matches, prev is a pre-existing node
             if (v == val && prev != null) {
+                // Swap next pointers of previous to the current next
                 prev.next = cur.next;
+                // Advance the current node, forget this one
                 cur = cur.next;
+            // Case 2: Value to be removed is at the start of the list
             } else if (v == val && prev == null ) {
+                // Just advance the head a node
                 head = head.next;
                 cur = cur.next;
+            // Value does not match, move forward
             } else {
                 prev = cur;
                 cur = cur.next;
