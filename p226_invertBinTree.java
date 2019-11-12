c0e650da2a3b7b8340d32dd23deea58f425ebccd
The Mysterious Stranger <52420930+Valenthyne@users.noreply.github.com>
2019-11-12 15:40:39 -0600

    Create p226_invertBinTree.java

---
+++ p226_invertBinTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return  null;
+        }
+        
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        
+        if (root.left != null) {
+            root.left = invertTree(root.left);
+        }
+        
+        if (root.right != null) {
+            root.right = invertTree(root.right);
+        }
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        return root;
+    }
+}
